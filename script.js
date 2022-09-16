9e95ae0604c9e9c1c7e96acf9ebba33e9e556bfb
Esteban Carter <carterea1999@gmail.com>
2022-09-16 00:30:06 -0400

    begginning work on  drawing cards function

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 import Deck from './deck.js'
 
-const hand = document.querySelector('.hand')
+const dealerHandValue = document.querySelector('.dealer-hand-value')
+const playerHandValue = document.querySelector('.player-hand-value')
+const hit = document.querySelector('.hit')
+const stand = document.querySelector('.stand')
+const dealerHand = document.querySelector('.dealer-hand')
+const playerHand = document.querySelector('.player-hand')
+const count = document.querySelector('.count')
+const theDeck = new Deck()
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards)
+let inRound
 
-hand.appendChild(deck.cards[0].getHTML())
+initialHand()
+function initialHand() {
+    inRound = true
+    const playerCard = theDeck.pop()
+    const computerCard = theDeck.pop()
+
+    playerHand.appendChild(playerCard.getHTML())
+
+}
+
+startGame()
+function startGame() {
+    const deck = new Deck()
+    deck.shuffle()
+    inRound = false
+
+    newRound()
+}
+
+function newRound(){
+    inRound = false
+    dealerHand.innerHTML = ''
+    playerHand.innerHTML = ''
+    count.innerHTML = ''
+}
