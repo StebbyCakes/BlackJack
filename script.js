86a1090a33643d1478781648e5d75f146f65d171
Esteban Carter <carterea1999@gmail.com>
2022-09-20 19:21:13 -0400

    Running out of cards starts new deck, win/lose/draw functions properly, working on displaying card values to player

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 import Deck from './deck.js'
 
+const CARD_VALUE_MAP = {
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10" : 10,
+    "J" : 11,
+    "Q" : 12,
+    "K" : 13,
+    "A" : 14,
+}
+
 const dealerHandValue = document.querySelector('.dealer-hand-value')
 const playerHandValue = document.querySelector('.player-hand-value')
 const dealerHand = document.querySelector('.dealer-hand')
@@ -7,6 +23,12 @@ const playerHand = document.querySelector('.player-hand')
 const count = document.querySelector('.count')
 
 document.getElementById("hitBtn").addEventListener('click', () => {
+    if (stop) {
+        startGame()
+        return
+    }
+
+
     if(inRound) {
         newRound()
     } else {
@@ -14,7 +36,7 @@ document.getElementById("hitBtn").addEventListener('click', () => {
     }
 })
 
-let inRound, theDeck
+let inRound, theDeck, stop
 
 startGame()
 function startGame() {
@@ -23,19 +45,9 @@ function startGame() {
     console.log(theDeck)
 
     inRound = false
+    stop = false
 
     newRound()
-}
-
-fullHand()
-function fullHand() {
-    inRound = true
-    console.log(theDeck)
-    const playerCard = theDeck.pop()
-    const computerCard = theDeck.pop()
-    playerHand.appendChild(playerCard.getHTML())
-    dealerHand.appendChild(computerCard.getHTML())
-
 }
 
 function newRound(){
@@ -44,10 +56,49 @@ function newRound(){
     playerHand.innerHTML = ''
     count.innerText = ''
 
+}
+
+fullHand()
+function fullHand() {
+    inRound = true
+    console.log(theDeck)
+    const playerCard = theDeck.pop()
+    const dealerCard = theDeck.pop()
+    playerHand.appendChild(playerCard.getHTML())
+    dealerHand.appendChild(dealerCard.getHTML())
+    // console.log(playerHand)
+
+    // const playerValue = playerHand.CARD_VALUE_MAP
+    // console.log(playerValue)
+
     updateDeck()
+
+    if (isRoundWinner(playerCard, dealerCard)) {
+        alert("Win")
+        let playerHandValue = CARD_VALUE_MAP[playerCard.value]
+        console.log(playerHandValue)
+    } else if (isRoundWinner(dealerCard, playerCard)){
+        alert("Lose")
+    } else {
+        alert("Draw")
+    }
+
+    if (isGameOver(theDeck)) {
+        alert("Game Over")
+        stop = true
+    }
 
 }
 
+
 function updateDeck() {
     theDeck.innerHTML = theDeck.numberOfCards
+}
+
+function isRoundWinner(cardOne, cardTwo) {
+    return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
+}
+
+function isGameOver(deck) {
+    return deck.numberOfCards === 0
 }
